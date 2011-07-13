ce7d53a0eda68ecfbbcb6c80805ea8df14921107
Łukasz Bandzarewicz <lucassus@gmail.com>
2011-07-13 17:07:09 +0200

    Added rails project.

---
+++ angular/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Angular::Application.config.secret_token = '41cd52b5f2e89367b111389b6220f05c12e4f21affc7265b3a65ffe61865ede1ba3f9a3676bff9e4e485ddc63628ab3b8487a23c9e2a279628a2f920d86a73cf'
