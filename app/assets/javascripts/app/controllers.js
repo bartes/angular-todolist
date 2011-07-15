76de366ee2655fe508eab0d093dd9fc0bdd4c001
Łukasz Bandzarewicz <lucassus@gmail.com>
2011-07-15 10:57:08 +0200

    Added simple todolist.

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -1,10 +1,59 @@
-/* App Controllers */
+function TodosController() {
+  this.estimates = [0, 1, 2, 3, 5, 8];
 
-
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
+  this.todos = [{
+    name: "Learn angular",
+    estimate: 8,
+    done: true
+  }, {
+    name: "Install java",
+    estimate: 2,
+    done: false
+  }, {
+    name: 'Uninstall ruby',
+    estimate: 3,
+    done: false
+  }];
 }
-MyCtrl2.$inject = [];
+
+TodosController.prototype = {
+  addTodo: function() {
+    if (this.todoName === "") { return false; }
+
+    this.todos.push({
+      name: this.todoName,
+      estimate: this.todoEstimate,
+      done: false
+    });
+
+    this.todoName = '';
+    this.todoEstimate = 0;
+  }
+};
+
+function TodoEditorController() {
+  this.editorEnabled = false;
+}
+
+TodoEditorController.prototype = {
+  enableEditor: function() {
+    this.editorEnabled = true;
+
+    this.todoName = this.todo.name;
+    this.todoEstimate = this.todo.estimate;
+  },
+
+  disableEditor: function() {
+    this.editorEnabled = false;
+  },
+
+  save: function() {
+    if (this.todoName === "") { return false; }
+
+    this.todo.name = this.todoName;
+    this.todo.estimate = this.todoEstimate;
+
+    this.disableEditor();
+  }
+};
+
